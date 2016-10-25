8883268e7e58bd4c4cbb9b100e5a47b4af229d69
hrl7 <hrlclb@gmail.com>
2016-10-26 01:45:54 +0900

    add psvr device info

--- js/debugger.js
+++ js/debugger.js
@@ -4,19 +4,19 @@ window.onload = main;
 function main() {
   const button = document.getElementById("find-device-button");
   const psvr = new PSVR();
-  const view = document.getElementById("data-view");
+  const deviceListView = document.getElementById("device-list-view");
+  const debugView = document.getElementById("debug-view");
   let deviceInfo = {};
 
   setInterval(()=>{
-    view.textContent = JSON.stringify(deviceInfo, null, "  ") + "\n" + JSON.stringify(psvr.data, null, "  "); 
+    deviceListView.textContent = JSON.stringify(psvr.devices, null, "  ");
+    debugView.textContent = JSON.stringify(deviceInfo, null, "  ") + "\n" + JSON.stringify(psvr.data, null, "  "); 
   }, 100);
 
   button.onclick = function() {
     psvr.findDevice()
     .then(() => {
-      for (let key in psvr.device) {
-        deviceInfo[key] = psvr.device[key];
-      }
+      deviceInfo = psvr.deviceInfo; 
     })
     .then(psvr.poll.bind(psvr))
   }

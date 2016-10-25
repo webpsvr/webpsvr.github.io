1715ac988539ba36630b0f4eb17b4c2e2130215b
hrl7 <hrlclb@gmail.com>
2016-10-25 15:09:44 +0900

    fix path

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.use("/client", express.static(path.join(__dirname, 'client')));
+app.use("/webusb-example/client", express.static(path.join(__dirname, 'client')));
 app.use("/stylesheets", express.static(path.join(__dirname, 'stylesheets')));
 app.listen(3333);
 console.log('listening on port 3333');

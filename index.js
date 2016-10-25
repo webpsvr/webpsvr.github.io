80494e3bb1044ef05e7a4ffef4902a6f56c729e1
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-10-26 03:53:24 +0900

    create 01.debugger directory

--- index.js
+++ index.js
@@ -13,5 +13,6 @@ app.use("/instruction", express.static(path.join(__dirname, 'instruction')));
 app.use("/lib", express.static(path.join(__dirname, 'lib')));
 app.use("/stylesheets", express.static(path.join(__dirname, 'stylesheets')));
 app.use("/assets", express.static(path.join(__dirname, 'assets')));
+app.use("/examples", express.static(path.join(__dirname, 'examples')));
 app.listen(3333);
 console.log('listening on port 3333');

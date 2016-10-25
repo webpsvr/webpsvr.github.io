e2047e6e038beb73e1e402fab939d360a6f1a62f
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-10-26 04:49:23 +0900

    create hello cube

--- js/helloCube.js
+++ js/helloCube.js
@@ -49,6 +49,8 @@ function animate() {
 
 	mesh.rotation.x += 0.005;
 	mesh.rotation.y += 0.01;
+	camera.rotation.x = acc.pitch;
+	camera.rotation.y = acc.yaw;
 
 	//renderer.render( scene, camera );
 	effect.render(scene, camera);
